0c1c351e146ab653dab1d547542546c27fb94d7b
ZinC0508 <18335432463@163.com>
2025-10-15 14:22:06 +0800

    DanceCurve v1.4 添加了进度条,修复了暂停不能保存进度的问题

--- Script/Hud.cs
+++ Script/Hud.cs
@@ -1,21 +1,31 @@
 using Godot;
 using System;
-using Godot.Collections;
 
-public partial class Hud : HBoxContainer
+namespace DanceCurve.Script;
+
+public partial class Hud : Control
 {
-    [Signal] public delegate void ChangeAudioEventHandler(string path);
+    //TODO: 播放列表
+    [Signal] public delegate void ChangeAudioEventHandler(string file);
     [Signal] public delegate void ShowFileWindowEventHandler();
     
     [Export] private float _idleTime = 3.0f;
 
     private Timer _idleTimer;
+    private ItemList _audioList;
     
     private string _selectedFile = ""; 
+    // private 
     
     public override void _Ready()
     {
         _idleTimer = GetNode<Timer>("IdleTimer");
+        _audioList = GetNode<ItemList>("AudioList");
+        
+		var window = GetTree().GetRoot().GetWindow();
+        window.MouseEntered += Active;
+        window.MouseExited += Hide;
+        
         Hide();
     }
 
@@ -23,6 +33,11 @@ public partial class Hud : HBoxContainer
     {
         if (@event is not InputEventMouse) return;
         
+        Active();
+    }
+
+    private void Active()
+    {
         Show();
         _idleTimer.Start(_idleTime);
     }
@@ -38,6 +53,12 @@ public partial class Hud : HBoxContainer
         if(_selectedFile == "") return;
         
         EmitSignalChangeAudio(_selectedFile);
+    }
+
+    private void UpdateAudioList()
+    {
+        var listY = Mathf.Clamp(_audioList.GetItemCount() * 27f + 8f, 0f, 400f);
+        _audioList.Size = new Vector2(_audioList.Size.X,listY);
     }
     private void OnIdleTimerTimeout() => Hide();
     private void OnNewAudioButtenPressed() => EmitSignalShowFileWindow();
--- Script/SoundEnvelopeController.cs
+++ Script/SoundEnvelopeController.cs
@@ -5,22 +5,40 @@ namespace DanceCurve.Script;
 
 public partial class SoundEnvelopeController : Node2D
 {
+    //TODO:添加贝塞尔曲线
+    [Signal]
+    public delegate void AudioProgressEventHandler(float progress);
+
+    [Signal]
+    public delegate void FpsCounterEventHandler(float fps);
+    [Signal]
+    public delegate void AudioFinishedEventHandler();
     
     [Export] public int WaveformSampleCount { get; set; } = 512;
 	[Export] public string CaptureBusName { get; set; } = "SubBus";
 	[Export] public int CaptureEffectIndex { get; set; } = 0;
 	[Export] public AudioStream PlayingAudio { get; set; } = GD.Load<AudioStream>("res://Audio/maldita.ogg");
 	[Export] public float AudioFixDelay { get; set; } = 0.1f;
-	
+	[Export] public AudioFinishModeEnum AudioFinishMode = AudioFinishModeEnum.Stop;
+	[Export] public float SleepTime = 3f;
+ 	
 	[ExportGroup("BufferA")]
 	[Export] public float UpdateInterval { get; set; } = 10f;
-	[Export(PropertyHint.Range,"0.3,0.5")] public float EnvelopeSmoothing = 0.4f;
+	[Export(PropertyHint.Range,"0.0,1.0")] public float EnvelopeSmoothing = 0.4f;
+	[Export] public float CompressK = 1.0f;
 	
 	[ExportGroup("Image")]
-	[Export] public Vector2I Resolution { get; set; } = new Vector2I(1280,720);
+	[Export] public Vector2I Resolution { get; set; } = new(640,480);
     
-    private readonly string _bufferShaderPath = "res://Shader/sound-envelope-buffer-a.gdshader";
-	private readonly string _imageShaderPath = "res://Shader/sound-envelope-image.gdshader";
+    private const string BufferShaderPath = "res://Shader/sound-envelope-buffer-a.gdshader";
+	private const string ImageShaderPath = "res://Shader/sound-envelope-image.gdshader";
+
+	public enum AudioFinishModeEnum
+	{
+		Stop,
+		Loop,
+		Next,
+	}
 
 	private TextureRect _outputRect;
     private SubViewport[] _viewports = new SubViewport[2];
@@ -29,12 +47,12 @@ public partial class SoundEnvelopeController : Node2D
     private AudioStreamPlayer _effectAudioPlayer;
     private AudioStreamPlayer _realAudioPlayer;
     private ShaderMaterial _imageMaterial;
-
     private Image _audioImage;
     private ImageTexture _audioTexture;
-    
     private int _frameCount;
 	private AudioEffectCapture _capture;
+	private float _lastProgressOfReal;
+	private float _totalProgress;
 	
 	public override void _Ready()
 	{
@@ -58,10 +76,11 @@ public partial class SoundEnvelopeController : Node2D
 			GD.PushWarning($"未找到 AudioEffectCapture：Bus='{CaptureBusName}', Index={CaptureEffectIndex}");
 		}
 		
-		GetTree().GetRoot().GetWindow().FilesDropped += OnFilesDropped;
-    }
+		var window = GetTree().GetRoot().GetWindow();
+		window.FilesDropped += OnFilesDropped;
+	}
 
-    public override void _PhysicsProcess(double delta)
+    public override void _Process(double delta)
     {
 		// 实时抓取音频并推送到音频纹理
 		if (_capture != null)
@@ -69,7 +88,7 @@ public partial class SoundEnvelopeController : Node2D
 			var framesAvail = _capture.GetFramesAvailable();
 			if (framesAvail > 0)
 			{
-				var vecs = _capture.GetBuffer(framesAvail); // PackedVector2Array (L,R)
+				var vecs = _capture.GetBuffer(framesAvail); //Vector2[512]
 				var target = WaveformSampleCount;
 				var outBuf = new float[target];
 				if (vecs.Length > 0)
@@ -77,17 +96,17 @@ public partial class SoundEnvelopeController : Node2D
 					var step = (double)vecs.Length / target;
 					for (var i = 0; i < target; i++)
 					{
+						//采样频率区间分割
 						var start = (int)Math.Floor(i * step);
 						var end = (int)Math.Floor((i + 1) * step);
-						if (end <= start) end = Math.Min(start + 1, vecs.Length);
 						var acc = 0.0;
 						for (var j = start; j < end; j++)
 						{
 							var v = vecs[j];
-							acc += 0.5 * (v.X + v.Y); // L/R -> mono
+							acc += 0.5 * (v.X + v.Y); //取均值合并左右声道采样数据
 						}
-						double denim = Math.Max(1, end - start);
-						outBuf[i] = (float)(acc / denim);
+						double denim = Math.Max(1, end - start);//区间长度
+						outBuf[i] = (float)(acc / denim);//返回该区间频率密度(均值)
 					}
 					PushAudioSamples(outBuf);
 				}
@@ -105,6 +124,9 @@ public partial class SoundEnvelopeController : Node2D
         bufMat.SetShaderParameter("iFrame", _frameCount);
         bufMat.SetShaderParameter("iChannel0", prevTexture);
         bufMat.SetShaderParameter("iChannel1", _audioTexture);
+        bufMat.SetShaderParameter("uUpdateInterval", UpdateInterval);
+        bufMat.SetShaderParameter("uEnvelopeSmoothing",EnvelopeSmoothing);
+        bufMat.SetShaderParameter("uCompressK",CompressK);
 
         // 确保绘制节点使用对应材质
         _passes[writeIndex].Material = bufMat;
@@ -117,6 +139,15 @@ public partial class SoundEnvelopeController : Node2D
         }
 
         _frameCount++;
+        EmitSignalFpsCounter((float)(1 / delta));
+        if(_totalProgress == 0f || !_realAudioPlayer.IsPlaying()) return;
+        UpdateProgress();
+    }
+
+    private void UpdateProgress()
+    {
+	    _lastProgressOfReal = _realAudioPlayer.GetPlaybackPosition() + (float)AudioServer.GetTimeSinceLastMix();
+	    EmitSignalAudioProgress(_lastProgressOfReal / _totalProgress);
     }
 
     // 供外部推送音频包络/波形的 API（长度应与 WaveformSampleCount 一致）
@@ -151,11 +182,11 @@ public partial class SoundEnvelopeController : Node2D
 
     private void SetupPingPong()
     {
-        var bufferShader = GD.Load<Shader>(_bufferShaderPath);
+        var bufferShader = GD.Load<Shader>(BufferShaderPath);
         if (bufferShader == null)
-            throw new InvalidOperationException($"Failed to load shader: {_bufferShaderPath}");
+            throw new InvalidOperationException($"Failed to load shader: {BufferShaderPath}");
 
-        for (int i = 0; i < 2; i++)
+        for (var i = 0; i < 2; i++)
         {
             var vp = new SubViewport
             {
@@ -185,15 +216,16 @@ public partial class SoundEnvelopeController : Node2D
 
     private void SetupImagePass()
     {
-        var imageShader = GD.Load<Shader>(_imageShaderPath);
+        var imageShader = GD.Load<Shader>(ImageShaderPath);
         if (imageShader == null)
-            throw new InvalidOperationException($"Failed to load shader: {_imageShaderPath}");
+            throw new InvalidOperationException($"Failed to load shader: {ImageShaderPath}");
 
         _imageMaterial = new ShaderMaterial { Shader = imageShader };
         _imageMaterial.SetShaderParameter("iResolution", (Vector2)Resolution);
         _outputRect.Material = _imageMaterial;
 
         // 将当前写入的 viewport 纹理显示出来（初始为 0）
+        //TODO:检查逻辑问题
         _outputRect.Texture = _viewports[0].GetTexture();
     }
 
@@ -206,6 +238,8 @@ public partial class SoundEnvelopeController : Node2D
 
     private void SetupAudioPlayer()
     {
+	    ArgumentNullException.ThrowIfNull(PlayingAudio);
+
 	    _effectAudioPlayer?.Free();
 	    _realAudioPlayer?.Free();
 	    _effectAudioPlayer = InitAudioPlayer(PlayingAudio, "SubBus");
@@ -215,6 +249,27 @@ public partial class SoundEnvelopeController : Node2D
 	    {
 		    _realAudioPlayer.Play();
 	    };
+	    _lastProgressOfReal = 0f;
+	    _totalProgress = (float)PlayingAudio.GetLength();
+	    _realAudioPlayer.Finished += OnAudioFinished;
+    }
+
+    private void OnAudioFinished()
+    {
+	    switch (AudioFinishMode)
+	    {
+		    case AudioFinishModeEnum.Stop:
+			    break;
+		    case AudioFinishModeEnum.Loop:
+			    ReloadPlayer();
+			    break;
+		    case AudioFinishModeEnum.Next:
+			    break;
+		    default:
+			    GD.PushError("Unsupported AudioFinishMode");
+			    return;
+	    }
+	    EmitSignalAudioFinished();
     }
 
     private AudioStreamPlayer InitAudioPlayer(AudioStream audioStream, string bus = "Master")
@@ -232,40 +287,49 @@ public partial class SoundEnvelopeController : Node2D
 	    {
 		    GD.PushWarning("Cant dropped more than one file");    
 	    }
+	    
 	    var file = files[0];
 	    GD.Print($"file loaded: {file}\n");
-	    switch (file.GetExtension())
+	    
+	    UpdateAudioStream(file);
+    }
+
+    public static AudioStream LoadAudioFromFile(string file)
+    {
+        ArgumentNullException.ThrowIfNull(file);
+        switch (file.GetExtension())
 	    {
 		    case "ogg":
+			    return AudioStreamOggVorbis.LoadFromFile(file);
 		    case "wav":
+			    return AudioStreamWav.LoadFromFile(file);
 		    case "mp3":
-			    UpdateAudioStream(file);
-			    break;
+			    return AudioStreamMP3.LoadFromFile(file);
 		    default:
-			    GD.PushWarning("Unsupported file type");
-			    return;
+			    GD.PushError("Unsupported file type");
+			    return null; 
 	    }
     }
 
+    public static AudioStream[] LoadAudioFromFile(string[] files)
+    {
+        ArgumentNullException.ThrowIfNull(files);
+        var audios = new AudioStream[files.Length];
+        
+        for (var i = 0; i < files.Length; i++)
+        {
+	        var audio = LoadAudioFromFile(files[i]);
+	        if (audio == null) i--;
+	        else audios[i] = audio;
+        }
+        
+        return audios;
+    }
+    
     private void UpdateAudioStream(string file)
     {
-	    AudioStream res;
-	    switch (file.GetExtension())
-	    {
-		    case "ogg":
-			    res = AudioStreamOggVorbis.LoadFromFile(file);
-			    break;
-		    case "wav":
-			    res = AudioStreamWav.LoadFromFile(file);
-			    break;
-		    case "mp3":
-			    res = AudioStreamMP3.LoadFromFile(file);
-			    break;
-		    default:
-			    GD.PushError("Unsupported file type");
-			    return;
-	    }
- 	    PlayingAudio = res;
+	    var audio = LoadAudioFromFile(file);
+	    PlayingAudio = audio;
 	    ReloadPlayer();
     }
     private void ReloadPlayer()
@@ -281,13 +345,15 @@ public partial class SoundEnvelopeController : Node2D
     {
 	    if (paused)
 	    {
+		    UpdateProgress();
+		    
 		    _effectAudioPlayer.Stop();
 		    _realAudioPlayer.Stop();
 	    }
 	    else if(!_realAudioPlayer.IsPlaying())
 	    {
-		    _effectAudioPlayer.Play();
-		    _realAudioPlayer.Play();
+		    _effectAudioPlayer.Play(_lastProgressOfReal - AudioFixDelay);
+		    _realAudioPlayer.Play(_lastProgressOfReal);
 	    }
     }
 }
